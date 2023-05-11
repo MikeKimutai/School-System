c46dc2ccfeb269874b091493b2497f026f409207
MikeKimutai <mikekimutai14@gmail.com>
2023-05-11 20:29:47 +0300

    School System

---
+++ js/main.js
@@ -0,0 +1,17 @@
+var nav = document.getElementById("menus");
+var close = document.getElementById("close");
+
+nav.addEventListener("click",function(){
+  document.getElementById("nav").style.display = "block";
+  document.getElementById("menus").style.display = "none";
+  document.getElementById("close").style.display = "block";
+})
+
+close.addEventListener("click",function(){
+  document.getElementById("nav").style.display = "none";
+  document.getElementById("menus").style.display = "block";
+  document.getElementById("close").style.display = "none";
+})
+
+var getDate = new Date().getFullYear();
+document.write('&copy; copyright  '+getDate+''+'Developed by Mike Kimutai';)
